87bd3b531c84c3549c16aa0b720c066a3d460255
Daniel De Guzman <50926547+DenDanDin@users.noreply.github.com>
2022-12-03 09:43:36 -0800

    installed bootstrap.
    bootstrap helps with styling.

---
+++ biggolf_frontend/src/pages/public/News.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewsList from "../../components/news-list";
+
+const News = () => {
+  return (
+    <>
+      <h1>News Page</h1>
+      <NewsList />
+    </>
+  )
+}
+
+export default News
---
+++ biggolf_frontend/src/pages/public/Players.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PlayersList from "../../components/players-list";
+
+const Players = () => {
+  return (
+    <>
+      <h1>Players Page</h1>
+      <PlayersList />
+    </>
+  )
+}
+
+export default Players;
