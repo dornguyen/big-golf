b0b8322e9d5f8bee5a267af59e2f24307a175b7a
Daniel De Guzman <50926547+DenDanDin@users.noreply.github.com>
2022-12-07 00:43:39 -0800

    scrapped Photos function, just added default slideshow

--- biggolf_frontend/src/pages/public/Photos.js
+++ biggolf_frontend/src/pages/public/Photos.js
@@ -1,11 +1,52 @@
 import React from 'react'
-import TournamentsList from "../../components/tournaments-list";
-
+import Slideshow from '../../components/Slideshow'
 const Photos = () => {
+  const slides = [
+    {
+      url: "http://localhost:3000/pictures/brookside.jpg",
+      titiel: "Photo 2"
+    },
+    {
+      url: "http://localhost:3000/pictures/debell.jpg",
+      titiel: "Photo 3"
+    },
+    {
+      url: "http://localhost:3000/pictures/golf-pic-1.jpg",
+      title: "Photo 4"
+    },
+    {
+      url: "http://localhost:3000/pictures/golf-pic-2.jpg",
+      title: "Photo 5"
+    },
+    {
+      url: "http://localhost:3000/pictures/golf-pic-3.jpg",
+      title: "Photo 6"
+    },
+    {
+      url: "http://localhost:3000/pictures/golfcourseoverview.jpg",
+      title: "Photo 7"
+    },
+    {
+      url: "http://localhost:3000/pictures/golftournament.jpg",
+      title: "Photo 8"
+    },
+    {
+      url: "http://localhost:3000/pictures/rangebucket.jpg",
+      title: "Photo 9"
+    }
+  ]
+
+  const containerStyles = {
+    width: '100%',
+    height: '500px',
+    margin: "0 auto",
+  }
   return (
     <>
       <h1>Photos Page</h1>
-      <TournamentsList />
+      <div style={containerStyles}>
+        <Slideshow slides={slides} />
+      </div>
     </>
   )
 }
