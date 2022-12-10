75915a27fc24592f79dbf47a5eee12031c2bdd1e
Daniel De Guzman <50926547+DenDanDin@users.noreply.github.com>
2022-12-10 14:26:12 -0800

    removed tutorial code

--- tutorial_frontend/src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "http://localhost:3000/api/v1/restaurants",
-    headers:{
-        "Content-type": "application/json"
-    }
-});
-
